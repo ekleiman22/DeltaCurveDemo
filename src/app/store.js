b19070b1f8984f9466e2c1291b8b84bf016f1a63
ekleiman22 <evgenykleiman@gmail.com>
2022-08-04 12:14:54 +0300

    Updates

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import demoReducer from '../demoSlice'
-
-export const store = configureStore({   
-    reducer: {
-        demo: demoReducer,
-    },
-});
-export default store;
